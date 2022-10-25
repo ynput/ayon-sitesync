b5fec8e138e2175278852aff62bfc0285c5243f5
Petr Kalis <petr.kalis@gmail.com>
2022-10-25 18:47:56 +0200

    Added frontend files
    Added pyproject.toml

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from typing import Any, Type
+
+from openpype.addons import BaseServerAddon
+
+
+class SiteSyncAddon(BaseServerAddon):
+    name = "sitesync"
+    title = "Site Sync Addon"
+    version = "1.0.0"
+
