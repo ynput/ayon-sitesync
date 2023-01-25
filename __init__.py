dc4c21a57ae89b22d7bd02b5afd534fcdd74e47f
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-25 10:24:55 +0100

    don't use custom 'get_local_client_info'

--- __init__.py
+++ __init__.py
@@ -47,25 +47,3 @@ class SiteSync(BaseServerAddon):
             method="DELETE",
         )
         logging.info("added endpoints")
-
-    def get_local_client_info(
-            self,
-            base_url: str | None = None,
-    ) -> dict[str, Any] | None:
-        """Returns information on local copy of the client code."""
-        logging.info("private:: {}".format(self.get_private_dir()))
-        if (pdir := self.get_private_dir()) is None:
-            return None
-        if base_url is None:
-            base_url = ""
-        client_zip = "{}_{}.zip".format(self.name, self.version)
-        logging.info("client_zip:: {}".format(client_zip))
-        local_path = os.path.join(pdir, client_zip)
-        logging.info("local_path:: {}".format(local_path))
-        if not os.path.exists(local_path):
-            return None
-        return {
-            "type": "http",
-            "path": f"{base_url}/addons/{self.name}/{self.version}/private/{client_zip}",  # noqa
-        }
-
