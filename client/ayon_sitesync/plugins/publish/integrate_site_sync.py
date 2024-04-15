2053baed5558f087e0de248bab002f9da47dc72d
Petr Kalis <petr.kalis@gmail.com>
2024-04-15 18:20:31 +0200

    Reworked addition of site status
    Not in ayon-core.integrate anymore, only here. There must be some process to `seed` state of all representations for a project if SiteSync is enabled in the middle.

--- client/ayon_sitesync/plugins/publish/integrate_site_sync.py
+++ client/ayon_sitesync/plugins/publish/integrate_site_sync.py
@@ -4,17 +4,8 @@ Each published representation might be marked to be synced to multiple
 sites. On some might be present (by default 'studio'), on some needs to
 be synchronized.
 
-It always state at least for 'studio' (even if SiteSync is disabled, in case
-of enabling, every artists can start synchronizing without any updates to DB).
-
-This plugins depends on logic in `integrate`. It is meant to be backward
-compatible. Approach should be refactored after v3 is gone.
 """
-import os
 import pyblish.api
-
-from ayon_sitesync.utils import SiteSyncStatus
-from openpype import AYON_SERVER_ENABLED
 
 
 class IntegrateSiteSync(pyblish.api.InstancePlugin):
@@ -24,9 +15,6 @@ class IntegrateSiteSync(pyblish.api.InstancePlugin):
     label = "Integrate Site Sync state"
 
     def process(self, instance):
-        if not AYON_SERVER_ENABLED:
-            return
-
         published_representations = instance.data.get(
             "published_representations")
         if not published_representations:
@@ -35,21 +23,15 @@ class IntegrateSiteSync(pyblish.api.InstancePlugin):
 
         context = instance.context
         project_name = context.data["projectEntity"]["name"]
-        modules_by_name = context.data["openPypeModules"]
-        sync_server_module = modules_by_name["sitesync"]
+        addons_by_name = context.data["ayonAddons"]
+        sitesync_addon = addons_by_name["sitesync"]
 
         for repre_id, inst in published_representations.items():  # noqa
-            new_site_files_status = {}
-            self.log.info("repre_id {}".format(repre_id))
-            for repre_file in inst["representation"]["files"]:
-                for site_info in repre_file["sites"]:
-                    site_name = site_info["name"]
-                    status = SiteSyncStatus.OK
-                    if not site_info.get("created_dt"):
-                        status = SiteSyncStatus.QUEUED
-                    new_site_files_status[site_name] = status
-                break  # after integrate status is same for all files
+            sites = sitesync_addon.compute_resource_sync_sites(
+                project_name=project_name
+            )
 
-            for site_name, status in new_site_files_status.items():
-                sync_server_module.add_site(project_name, repre_id, site_name,
-                                            status=status)
+            for site_info in sites:
+                sitesync_addon.add_site(project_name, repre_id,
+                                        site_info["name"],
+                                        status=site_info["status"])
