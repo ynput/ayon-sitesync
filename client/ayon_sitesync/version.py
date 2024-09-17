ad1ab7b123d88f9b26228b87ae0bb608eb53cefa
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-09-17 14:33:26 +0200

    bump version to '1.2.1-dev.1'

--- client/ayon_sitesync/version.py
+++ client/ayon_sitesync/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring sitesync addon version."""
-__version__ = "1.2.0"
+__version__ = "1.2.1-dev.1"
--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 """Package declaring addon version."""
 name = "sitesync"
 title = "SiteSync"
-version = "1.2.0"
+version = "1.2.1-dev.1"
 client_dir = "ayon_sitesync"
 
 ayon_required_addons = {
